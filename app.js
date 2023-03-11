6b596e19bb7dd2c631494bce6bb0012ee3cf98a2
Charan-04 <77110886+Charan-04@users.noreply.github.com>
2023-03-11 22:42:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+// Smooth scrolling for internal links
+const smoothScroll = function(target, duration) {
+    const targetElement = document.querySelector(target);
+    const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    let startTime = null;
+  
+    const animation = function(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    };
+  
+    const ease = function(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+    };
+  
+    requestAnimationFrame(animation);
+  };
+  
+  const links = document.querySelectorAll('nav a');
+  links.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const target = link.getAttribute('href');
+      smoothScroll(target, 1000);
+    });
+  });
+  
